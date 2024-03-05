fd44f2932221dde748a8f21b3d39400a5d4b9389
Aivinni <Aivinni1219@gmail.com>
2024-03-05 10:17:46 -0500

    Initial commit

---
+++ src/VehicleRunner.java
@@ -0,0 +1,26 @@
+public class VehicleRunner {
+    public static void main(String[] args) {
+        // testing all available methods on a Vehicle object
+        Vehicle myVehicle = new Vehicle("Generic Vehicle", 6);
+        System.out.println(myVehicle.getName());
+        System.out.println(myVehicle.getWheels());
+        myVehicle.move(10);
+        myVehicle.turn();
+        myVehicle.brake();
+
+        // testing all available methods on a Car object
+        Car myCar = new Car("Honda Civic", 4, "leather");
+        System.out.println(myCar.getName()); // inherited method
+        System.out.println(myCar.getWheels()); // inherited method
+        System.out.println(myCar.getFabric()); // method ONLY available on Car objects
+        myCar.move(30); // inherited method
+        myCar.turn(); // inherited method
+        myCar.brake(); // inherited method
+        myCar.honk(); // method ONLY available on Car objects
+        myCar.lockDoors(); // method ONLY available on Car objects
+
+        // ---- WRITE YOUR BICYCLE TEST CODE BELOW ----
+
+
+    }
+}
